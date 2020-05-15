eb0062c1f9f13de13f548ee268431756a098f594
jemc300x <jemc300x@gmail.com>
2020-05-14 21:19:41 -0500

    first commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,40 @@
+
+<style>
+  .header{
+    max-width: var(--desktop-width);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100px;
+    margin: 0px auto;
+    padding: 0 5%;
+  }
+
+  .header-logo{
+    width: 100px;
+  }
+
+  .header-navbar{
+    list-style: none;
+    display: inline-flex;
+    font-family: var(--font-family);
+  }
+
+  .header-navbar li {
+    margin-right: 10px;
+  }
+
+  .header-navbar a {
+    text-decoration: none;
+  }
+
+</style>
+
+<header class="header">
+  <img class="header-logo" src="/assets/img/logo.svg" alt="logo">
+  <ul class="header-navbar">
+    <li><a href="/">Feature</a></li>
+    <li><a href="/">Team</a></li>
+    <li><a href="/">Sing In</a></li>
+  </ul>
+</header>
---
+++ src/containers/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Header from '../components/Header.svelte';
+</script>
+
+<style>
+  :global(:root){
+    --font-family: 'Raleway', sans-serif;
+    --desktop-width: 1440px;
+    --mobile-width: 375px;
+    /* Color Primary */
+    --Very-Dark-Blue: hsl(243, 87%, 12%);
+    --Desaturated-Blue: hsl(238, 22%, 44%);
+    /* Color Accent */
+    --Bright-Blue: hsl(224, 93%, 58%);
+    --Moderate-Cyan: hsl(170, 45%, 43%);
+    /* Color Neutral */
+    --Light-Grayish-Blue: #f8f8fe;
+    --Light-Gray: hsl(0, 0%, 75%);
+  }
+
+  :global(body){
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 400;
+    margin: 0 auto;
+    color: var(--Very-Dark-Blue);
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
+<Header></Header>
